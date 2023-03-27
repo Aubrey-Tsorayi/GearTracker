9c974cca392e69cef5b6a894a00f5018dd678c26
Tutsirayi Aubrey Tsorayi <tutsirayit.tt@gmail.com>
2023-03-27 14:03:38 +0200

    add-equipment

--- controllers/equipment/add-equipment.php
+++ controllers/equipment/add-equipment.php
@@ -1,20 +1,18 @@
 <?php
 require ("../../config/db-config.php");
 
-if (isset($_POST['submit'])){
+if (isset($_POST['name'])){
 
     // getting info from form
     $name = $_POST['name'];
     $code =$_POST['code'];
-    $sport = $_POST['type'];
-    $available = $_POST['quantity_available'];
+    $sport = $_POST['sport'];
     $quantity = $_POST['quantity'];
-    
-    
-   
-    //$passw = $_POST['password'];
+    $quantity_available = $_POST['equipment'];
+    $description = $_POST['description'];
 
-    $query = "INSERT INTO `equipment`(`code`, `name`, `sport`, `quanity`/*, `available`, `Sport`*/) VALUES ('$code','$name','$sport',' $quantity'/*,'$number','$passw'*/)";
+    $query = "INSERT INTO `equipment`(`name`, `code`, `sport`, `quantity`, `quantity_available`, `description`) 
+    VALUES ('$name','$code','$sport','$quantity','$quantity_available','$description')";
 
     $request = mysqli_query($conn, $query);
 
@@ -22,13 +20,13 @@ if (isset($_POST['submit'])){
         echo '<script> window.location.href="../../views/equipment/list-equipment.php"; </script>';
     }
     else{
-        echo '<script> alert("Equipment Addition failed.");
+        echo '<script> alert("User registration failed.");
         window.location.href = "../../views/equipment/add-equipment.php";
          </script>';
     }
 }
 else{
     echo '<script> alert("Database failed");
-        window.location.href = "../../views/auth/login.php";
+        window.location.href = "../../views/equipment/add-equipment.php";
          </script>';
 }
