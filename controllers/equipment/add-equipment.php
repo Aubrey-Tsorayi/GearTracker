f770ba33b4be2fc0cfd7ab6ded23c31fcfecb976
Tutsirayi Aubrey Tsorayi <tutsirayit.tt@gmail.com>
2023-04-10 13:51:58 +0200

    existing equipment code only updates quantity

--- controllers/equipment/add-equipment.php
+++ controllers/equipment/add-equipment.php
@@ -1,31 +1,46 @@
 <?php
 session_start();
-require ("../../config/db-config.php");
+require("../../config/db-config.php");
 
-if (isset($_POST['name'])){
+if (isset($_POST['name'])) {
     session_start();
     // getting info from form
     $name = $_POST['name'];
-    $code =$_POST['code'];
+    $code = $_POST['code'];
     $sport = $_POST['sport'];
     $quantity = $_POST['equipment'];
     $description = $_POST['description'];
 
-    $query = "INSERT INTO `equipment`(`equipment_code`, `equipment_name`, `sport`, `quantity`, `quantity_available`, `description`) 
-    VALUES ('$code','$name','$sport','$quantity','$quantity','$description')";
-
-    $request = mysqli_query($conn, $query);
-
-    if($request){
-        echo '<script> window.location.href="../../views/equipment/list-equipment.php"; </script>';
+    $code_names = array();
+    $codes = mysqli_query($conn, "SELECT `equipment_code` FROM `equipment`");
+    while ($row = mysqli_fetch_assoc($result)) {
+        $code_names[] = $row['equipment_code'];
     }
-    else{
-        echo '<script> alert("User registration failed.");
+
+    if (in_array($code_to_update, $equipment_codes)) {
+        $quantiy_update = mysqli_query($conn, "UPDATE `equipment` SET `quantity` = `quantity` + '$quantity' WHERE `equipment_code` = '$code'");
+        if ($quantiy_update) {
+            echo '<script> window.location.href="../../views/equipment/list-equipment.php"; </script>';
+        } else {
+            echo '<script> alert("Make sure details are correct.");
+            window.location.href = "../../views/equipment/add-equipment.php";
+             </script>';
+        }
+    } else {
+        $query = "INSERT INTO `equipment`(`equipment_code`, `equipment_name`, `sport`, `quantity`, `quantity_available`, `description`) 
+        VALUES ('$code','$name','$sport','$quantity','$quantity','$description')";
+
+        $request = mysqli_query($conn, $query);
+
+        if ($request) {
+            echo '<script> window.location.href="../../views/equipment/list-equipment.php"; </script>';
+        } else {
+            echo '<script> alert("Make sure details are correct");
         window.location.href = "../../views/equipment/add-equipment.php";
          </script>';
+        }
     }
-}
-else{
+} else {
     echo '<script> alert("Database failed");
         window.location.href = "../../views/equipment/add-equipment.php";
          </script>';
