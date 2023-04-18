98d2e24cf5319bb3a5ca045162c785635dcab66c
Tutsirayi Aubrey Tsorayi <tutsirayit.tt@gmail.com>
2023-04-18 12:40:07 +0200

    can't take out zero

--- controllers/take-out/take-out-equipment.php
+++ controllers/take-out/take-out-equipment.php
@@ -12,64 +12,71 @@ if (isset($_POST['submit'])) {
     $datetime = date('Y-m-d H:i:s');
     $user = $_SESSION['user_id'];
 
-    //notification message
-    $title = "Take Out";
-    $message = "You have taken out $quantity $equipment";
-
-
-    // Generate the new reference code for take out ID
-    $result = mysqli_query($conn, 'SELECT COUNT(`take_out_id`) FROM `take_out`');
-    $max_ref_num = mysqli_fetch_row($result)[0]; 
-    if ($max_ref_num == 0) {
-        $reference = 'TAKEOUT0';
+    if ($quantity == 0) {
+        echo '<script> alert("Quantity cannot be 0");
+        window.location.href = "../../views/take_out/take-out-equipment.php";
+         </script>';
     } else {
-        $new_ref_num = $max_ref_num + 1;
-        // Generate the new reference code
-        $reference = 'TAKEOUT' . $new_ref_num;
-    }
+        //notification message
+        $title = "Take Out";
+        $message = "You have taken out $quantity $equipment";
 
 
-    // getting available quantity and updating it with the new available
-    $available = mysqli_query($conn, "SELECT `quantity_available` FROM `equipment` WHERE `equipment_code` = '$equipment'");
-    $available_quantity = mysqli_fetch_row($available)[0];
+        // Generate the new reference code for take out ID
+        $result = mysqli_query($conn, 'SELECT COUNT(`take_out_id`) FROM `take_out`');
+        $max_ref_num = mysqli_fetch_row($result)[0];
+        if ($max_ref_num == 0) {
+            $reference = 'TAKEOUT0';
+        } else {
+            $new_ref_num = $max_ref_num + 1;
+            // Generate the new reference code
+            $reference = 'TAKEOUT' . $new_ref_num;
+        }
 
-    if ($available_quantity == 0) {
-        echo '<script> alert("No available quantity");
+
+        // getting available quantity and updating it with the new available
+        $available = mysqli_query($conn, "SELECT `quantity_available` FROM `equipment` WHERE `equipment_code` = '$equipment'");
+        $available_quantity = mysqli_fetch_row($available)[0];
+
+        if ($available_quantity == 0) {
+            echo '<script> alert("No available quantity");
         window.location.href = "../../views/take_out/take-out-equipment.php";
          </script>';
-    } else if ($available_quantity < $quantity) {
-        echo '<script> alert("Quantity requested is more than available");
+        } else if ($available_quantity < $quantity) {
+            echo '<script> alert("Quantity requested is more than available");
         window.location.href = "../../views/take_out/take-out-equipment.php";
          </script>';
-    } else if ($quantity > 0){
-        $new_quantity = $available_quantity - $quantity;
-        // updating the new quantity in the equipment using code
-        $update = mysqli_query($conn, "UPDATE `equipment` SET `quantity_available`='$new_quantity' WHERE `equipment_code` = '$equipment'");
+        } else if ($quantity > 0) {
+            $new_quantity = $available_quantity - $quantity;
+            // updating the new quantity in the equipment using code
+            $update = mysqli_query($conn, "UPDATE `equipment` SET `quantity_available`='$new_quantity' WHERE `equipment_code` = '$equipment'");
 
-        $equipment_query = mysqli_query($conn, "SELECT `equipment_name` FROM `equipment` WHERE `equipment_code` = '$equipment'");
-        $equipment_name = mysqli_fetch_row($equipment_query)[0];
+            $equipment_query = mysqli_query($conn, "SELECT `equipment_name` FROM `equipment` WHERE `equipment_code` = '$equipment'");
+            $equipment_name = mysqli_fetch_row($equipment_query)[0];
 
-        $query = "INSERT INTO `take_out`(`take_out_id`, `equipment_code`, `equipment_name`, `user_id`, `date`, `quantity`)
+            $query = "INSERT INTO `take_out`(`take_out_id`, `equipment_code`, `equipment_name`, `user_id`, `date`, `quantity`)
         VALUES ('$reference','$equipment', '$equipment_name', '$user','$current_date','$quantity')";
 
 
-        $request = mysqli_query($conn, $query);
+            $request = mysqli_query($conn, $query);
 
-        if ($request) {
-            $notification = mysqli_query($conn, "INSERT INTO `notifications`(`title`, `message`, `date`)
+            if ($request) {
+                $notification = mysqli_query($conn, "INSERT INTO `notifications`(`title`, `message`, `date`)
             VALUES ('$title', '$message', '$datetime')");
-            //require("../email/mailer.php");
-            echo '<script> window.location.href="../../views/take_out/equipment-taken.php"; </script>';
+                //require("../email/mailer.php");
+                echo '<script> window.location.href="../../views/take_out/equipment-taken.php"; </script>';
+            } else {
+                echo '<script> alert("Take Out failed");
+        window.location.href = "../../views/take_out/take-out-equipment.php";
+         </script>';
+            }
         } else {
-            echo '<script> alert("Take Out failed");
+            '<script> alert("Error");
         window.location.href = "../../views/take_out/take-out-equipment.php";
          </script>';
         }
-    }else{
-        '<script> alert("Error");
-        window.location.href = "../../views/take_out/take-out-equipment.php";
-         </script>';
     }
+
 } else {
     echo '<script> alert("Database failed");
         window.location.href = "../../views/take_out/take-out-equipment.php";
